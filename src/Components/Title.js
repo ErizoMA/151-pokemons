635e55ba615f3a18ea490a3a9bbbf5dfed808de2
Erick Marcelo <emarceloa@uni.pe>
2021-12-24 19:28:07 -0500

    Create Title component

---
+++ src/Components/Title.js
@@ -0,0 +1,21 @@
+import styled from "@emotion/styled";
+
+function Title() {
+  return (
+    <P>
+      800 <Span>Pokemons</Span> for you to choose your favorite
+    </P>
+  );
+}
+
+const P = styled.p`
+  text-align: center;
+  letter-spacing: 3px;
+  font-weight: 400;
+  font-size: 35px;
+  margin: 0 5%;
+`;
+const Span = styled.span`
+  font-weight: 700;
+`;
+export default Title;
